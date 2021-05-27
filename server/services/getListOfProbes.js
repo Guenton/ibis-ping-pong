406a14bec6148a48ef7d9ec554435ecf245e3d40
Guenton Brooks <guenton.brooks@ibis-management.com>
2021-05-27 15:55:36 -0400

    updated probes call

--- server/services/getListOfProbes.js
+++ server/services/getListOfProbes.js
@@ -1,17 +1,10 @@
-const ping = require("ping");
+const Probes = require("../models/Probes");
 const catchErr = require("../utils/catchErr");
-const logger = require("../utils/logger");
 
 const getListOfProbes = async (req, res) => {
-  const hosts = req.body.hosts || [];
-  // const probes = [];
   try {
-    const probes = Promise.all(hosts.map((host) => ping.promise.probe(host)));
-    // for (let host of hosts) {
-    //   let probe = await ping.promise.probe(host);
-    //   probes.push(probe);
-    // }
-    // const probe = await ping.promise.probe(host);
+    const probes = await Probes.findAll();
+
     res.status(200).json(probes);
   } catch (err) {
     catchErr(err, res);
